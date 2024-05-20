f2d3ffda936bacd89760bbb31a16abcabb80966f
Rakibul Islam <rakib.ca@gmail.com>
2024-05-20 10:26:26 -0600

    ContactCard component & props created

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
+import ContactCard from "./ContactCard";
+
 function App() {
   return (
     <ul>
-      <li className="contact-card">
-        <h2>Sunita Kumar</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>Electrical Engineer</dd>
-          <dt>Email</dt>
-          <dd>sunita.kumar@acme.co</dd>
-        </dl>
-      </li>
-      <li className="contact-card">
-        <h2>Henderson G. Sterling II</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>Receptionist</dd>
-          <dt>Email</dt>
-          <dd>henderson-the-second@acme.co</dd>
-        </dl>
-      </li>
-      <li className="contact-card">
-        <h2>Aoi Kobayashi</h2>
-        <dl>
-          <dt>Job</dt>
-          <dd>President</dd>
-          <dt>Email</dt>
-          <dd>kobayashi.aoi@acme.co</dd>
-        </dl>
-      </li>
+      <ContactCard
+        name="Sunita Kumar"
+        job="Electrical Engineer"
+        email="sunita.kumar@acme.co"
+      />
+      <ContactCard
+        name="Henderson G. Sterling II"
+        job="Receptionist"
+        email="henderson-the-second@acme.co"
+      />
+      <ContactCard
+        name="Aoi Kobayashi"
+        job="President"
+        email="kobayashi.aoi@acme.co"
+      />
     </ul>
   );
 }
